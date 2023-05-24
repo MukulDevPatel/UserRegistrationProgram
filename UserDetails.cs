6cf6df8ad54ddaaa27aaaaec8c34a4d503a1ab11
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-24 20:54:40 +0530

    [Mukul Dev] Add: We are adding user email in this program

--- UserDetails.cs
+++ UserDetails.cs
@@ -9,14 +9,14 @@ namespace UserRegistrationProblem
 {
     public class UserDetails
     {
-        public const string FirstName = "^[A-Z][a-z]{1,4}$";
-        public const string LastName = "^[A-Z][a-z]{1,4}$";
+        public const string NAME = "^[A-Z][a-z]{1,4}$";
+        public const string EMAIL = "^[a-zA-Z]+[.+-]{0,1}[a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
         public static void UserData()
         {
             //For enter to first name of user
             Console.Write("Enter first name of user: ");
             string firstName = Console.ReadLine();
-            if (Regex.IsMatch(firstName, FirstName))
+            if (Regex.IsMatch(firstName, NAME))
             {
                 Console.WriteLine("Valid first name of user");
             }else
@@ -27,13 +27,25 @@ namespace UserRegistrationProblem
             //For enter to last name of user
             Console.Write("\nEnter last name of user: ");
             string lastName = Console.ReadLine();
-            if (Regex.IsMatch(lastName, LastName))
+            if (Regex.IsMatch(lastName, NAME))
             {
                 Console.WriteLine("Valid last name of user");
             }
             else
             {
                 Console.WriteLine("Invalid enterd name");
+            }
+
+            //For enter to last name of user
+            Console.Write("\nEnter email of user: ");
+            string email = Console.ReadLine();
+            if (Regex.IsMatch(email, EMAIL))
+            {
+                Console.WriteLine("Valid email of user");
+            }
+            else
+            {
+                Console.WriteLine("Invalid enterd email");
             }
         }
     }
