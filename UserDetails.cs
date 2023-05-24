7d0f0df516ae95f9fefb1162238b4dbce6393785
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-24 21:05:19 +0530

    [Mukul Dev] Add: We are adding user number in this program

--- UserDetails.cs
+++ UserDetails.cs
@@ -9,43 +9,19 @@ namespace UserRegistrationProblem
 {
     public class UserDetails
     {
-        public const string NAME = "^[A-Z][a-z]{1,4}$";
-        public const string EMAIL = "^[a-zA-Z]+[.+-]{0,1}[a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+        public const string NUMBER = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
         public static void UserData()
         {
-            //For enter to first name of user
-            Console.Write("Enter first name of user: ");
-            string firstName = Console.ReadLine();
-            if (Regex.IsMatch(firstName, NAME))
+            //For enter to number of user
+            Console.Write("\nEnter number of user: ");
+            string number = Console.ReadLine();
+            if (Regex.IsMatch(number, NUMBER))
             {
-                Console.WriteLine("Valid first name of user");
-            }else
-            {
-                Console.WriteLine("Invalid enterd name");
-            }
-
-            //For enter to last name of user
-            Console.Write("\nEnter last name of user: ");
-            string lastName = Console.ReadLine();
-            if (Regex.IsMatch(lastName, NAME))
-            {
-                Console.WriteLine("Valid last name of user");
+                Console.WriteLine("Valid number of user");
             }
             else
             {
-                Console.WriteLine("Invalid enterd name");
-            }
-
-            //For enter to last name of user
-            Console.Write("\nEnter email of user: ");
-            string email = Console.ReadLine();
-            if (Regex.IsMatch(email, EMAIL))
-            {
-                Console.WriteLine("Valid email of user");
-            }
-            else
-            {
-                Console.WriteLine("Invalid enterd email");
+                Console.WriteLine("Invalid enterd number");
             }
         }
     }
