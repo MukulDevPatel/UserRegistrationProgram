efca09c3695175f74ff9ad5d835ffc9aaa559a58
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-24 19:46:12 +0530

    [Mukul Dev] Add: We are add user last name and it will be start cap charcter and has 3 character

--- UserDetails.cs
+++ UserDetails.cs
@@ -9,15 +9,29 @@ namespace UserRegistrationProblem
 {
     public class UserDetails
     {
-        public const string Name = "^[A-Z][a-z]{1,4}$";
+        public const string FirstName = "^[A-Z][a-z]{1,4}$";
+        public const string LastName = "^[A-Z][a-z]{1,4}$";
         public static void UserData()
         {
+            //For enter to first name of user
             Console.Write("Enter first name of user: ");
-            string name = Console.ReadLine();
-            if (Regex.IsMatch(name,Name))
+            string firstName = Console.ReadLine();
+            if (Regex.IsMatch(firstName, FirstName))
             {
                 Console.WriteLine("Valid first name of user");
             }else
+            {
+                Console.WriteLine("Invalid enterd name");
+            }
+
+            //For enter to last name of user
+            Console.Write("\nEnter last name of user: ");
+            string lastName = Console.ReadLine();
+            if (Regex.IsMatch(lastName, LastName))
+            {
+                Console.WriteLine("Valid last name of user");
+            }
+            else
             {
                 Console.WriteLine("Invalid enterd name");
             }
